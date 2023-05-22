8346381c2c5541fd19f7258231a8fda69ffbbc59
Asilbekyan <asilbekyanlilit2@gmail.com>
2023-05-22 11:51:48 +0400

    Update spectral.py

--- Clustering/spectral.py
+++ Clustering/spectral.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt 
 
 from sklearn.cluster import KMeans
@@ -6,8 +7,9 @@ from sklearn.datasets import make_blobs
 
 
 class SpectralClustering:
-    def __init__(self, n_clusters=3):
+    def __init__(self, n_clusters, sigma=1):
         self.n_clusters = n_clusters
+        self.sigma = sigma
         self.labels_ = None
         self.L = None
 
@@ -19,10 +21,9 @@ class SpectralClustering:
         D = np.diag(W.sum(axis=0))
         self.L = D - W
         
-        eigenvalues, eigenvectors = np.linalg.eig(self.L)
-        H = eigenvectors[:self.n_clusters].T
-
-        kmeans = KMeans(n_clusters=self.n_clusters).fit(X)
+        eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(self.L, k=self.n_clusters+1, which='SM')
+        H = eigenvectors
+        kmeans = KMeans(n_clusters=self.n_clusters).fit(H)
         self.labels_ = kmeans.labels_
 
         return self.labels_
@@ -32,24 +33,24 @@ class SpectralClustering:
         n = X.shape[0]
         
         W = np.zeros((n, n))
-        var = np.median(X)
 
         for i in range(n):
-            for j in range(n):
-                if i == j:
-                    W[i][j] = 0
-                else:
-                    W[i][j] = np.exp(np.linalg.norm(X[i] - X[j]) ** 2 / (2 * var**2))
-            
+            for j in range(i, n):
+                d = np.linalg.norm(X[i] - X[j])
+                W[i][j] = d
+                W[j][i] = d
+        
+        W = np.exp(-W**2 / (2 * self.sigma**2))
+
         return W
 
 
 
 if __name__ == '__main__':
-    X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+    X, y_true = make_blobs(n_samples=500, centers=4, cluster_std=0.60, random_state=42)
 
     model = SpectralClustering(n_clusters=4)
     y_pred = model.fit_predict(X)
-
+    
     plt.scatter(X[:, 0], X[:, 1], c=y_pred, s=50, cmap='viridis')
     plt.show()
